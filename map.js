81651547ccf67e850235112e66c1a2e8a1bccffc
prashanthk02 <prashanth.k369@gmail.com>
2022-03-10 18:15:13 -0500

    lint

--- map.js
+++ map.js
@@ -1,13 +1,13 @@
-const eqArrays = function (arr1, arr2) {
-  if(arr1.length !== arr2.length){
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]){
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
-    }
-    return true;
+  }
+  return true;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
@@ -28,16 +28,16 @@ const map = function(array, callback) {
   for (let item of array) {
     results.push(callback(item));
   }
-  return results
-}
+  return results;
+};
 
 const results1 = map(words, word => word[0]);
 // console.log(results1);
 
-const results2 = map(num, x => x+1);
+const results2 = map(num, x => x + 1);
 // console.log(results2);
 
-const results3 = map(nums, y => y+1);
+const results3 = map(nums, y => y + 1);
 // console.log(results3);
 
 assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]); //pass
