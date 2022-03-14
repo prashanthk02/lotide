2eb0da4e3a74250c0f41c0a900631d7ca91199a7
prashanthk02 <prashanth.k369@gmail.com>
2022-03-14 17:31:57 -0400

    updated and created index

--- letterPositions.js
+++ letterPositions.js
@@ -1,22 +1,5 @@
-const eqArrays = function (arr1, arr2) {
-  if(arr1.length !== arr2.length){
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]){
-      return false;
-    }
-    }
-    return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //letter positions function
 const letterPositions = function(sentence) {
@@ -36,3 +19,5 @@ const letterPositions = function(sentence) {
 
 console.log(letterPositions('hello'));
 assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;
